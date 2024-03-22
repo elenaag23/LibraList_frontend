6b7ba617ed461a4e37f654fd7538aa5dc29dc6ac
elenaag23 <elena-alexandra.georgescu@s.unibuc.ro>
2024-03-22 22:41:14 +0200

    ResolvedPdfFunctionality

--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -1,23 +1,64 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
-const PDFViewer = ({ pdfUrl }) => {
-  const iframeRef = useRef(null);
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-  useEffect(() => {
-    if (iframeRef.current) {
-      iframeRef.current.src = pdfUrl;
+const MyPDFViewer = ({ pdfUrl }) => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [selectedText, setSelectedText] = useState("");
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const handleTextSelection = () => {
+    const selection = window.getSelection();
+    setSelectedText(selection.toString());
+  };
+
+  const goToPreviousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
     }
-  }, [pdfUrl]);
+  };
+
+  const goToNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
 
   return (
-    <iframe
-      ref={iframeRef}
-      title="PDF Viewer"
-      width="100%"
-      height="570px"
-      style={{ border: "5px solid #6d7fcc" }}
-    />
+    <div style={{ display: "flex" }}>
+      <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page
+          pageNumber={pageNumber}
+          onMouseUp={handleTextSelection}
+          className="pdf-page"
+          renderTextLayer={true}
+          loading={<div>Wait, your pdf is loading...</div>}
+        />
+      </Document>
+
+      <div className="pagesDiv">
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+        <div style={{ paddingLeft: "15px" }}>
+          <button disabled={pageNumber <= 1} onClick={goToPreviousPage}>
+            Previous
+          </button>
+          <button disabled={pageNumber >= numPages} onClick={goToNextPage}>
+            Next
+          </button>
+        </div>
+        <div className="selected-text">Selected Text: {selectedText}</div>
+      </div>
+    </div>
   );
 };
 
-export default PDFViewer;
+export default MyPDFViewer;
