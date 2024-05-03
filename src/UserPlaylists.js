4f76bd120e3fb81c7a8ff1c5c4c8efa04d9ce205
elenaag23 <elena-alexandra.georgescu@s.unibuc.ro>
2024-05-04 01:17:04 +0300

    added yt iframe

--- src/UserPlaylists.js
+++ src/UserPlaylists.js
@@ -10,13 +10,14 @@ const UserPlaylists = () => {
   const userMail = localStorage.getItem("userMail");
 
   useEffect(() => {
+    console.log("entered user playlist");
     $("#playlistButton").addClass("selected");
     setLoading(true);
     getUserPlaylists();
   }, []);
 
   const getUserPlaylists = () => {
-    fetch(`http://127.0.0.1:8000/userPlaylists`, {
+    fetch(`http://127.0.0.1:8000/userPlaylists2`, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -51,6 +52,8 @@ const UserPlaylists = () => {
         <span>Playlists</span>
       </div>
 
+      <div></div>
+
       <div
         className="row"
         style={{ width: "97vw", height: "100vh", margin: "16px" }}
@@ -62,9 +65,18 @@ const UserPlaylists = () => {
         >
           {playlists &&
             playlists.map((playlist, index) => (
-              <div key={index} className="playlistsList">
-                <div className="songItem">
-                  <span className="songFont">{playlist.playlistName}</span>
+              <div
+                key={index}
+                className="playlistsList"
+                style={{ width: "95%" }}
+              >
+                <div
+                  className="songItem"
+                  style={{ width: "100%", paddingBottom: "15px" }}
+                >
+                  <span className="songFont2" style={{ color: "white" }}>
+                    {playlist.playlistName}
+                  </span>
                 </div>
               </div>
             ))}
@@ -72,21 +84,38 @@ const UserPlaylists = () => {
         <div
           className="col-9 row"
           style={{
-            backgroundColor: "#d9def2",
+            backgroundColor: "#6d7fcc",
             borderLeft: "2px solid #a1acde",
           }}
         >
-          <div className="col-8" style={{ height: "100vh" }}></div>
+          <div className="col-8" style={{ height: "100vh" }}>
+            <div className="video-container">
+              <iframe
+                width="560"
+                height="315"
+                src={"https://www.youtube.com/embed/xo1VInw-SKc"}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
           <div className="col-4" style={{ height: "100vh" }}>
+            <div className="songTitle">
+              <span>{playlists[0]["playlistName"]} songs</span>
+            </div>
             {songs &&
               songs[playlists[0]["playlistId"]] &&
               songs[playlists[0]["playlistId"]].map((song, index) => (
                 <div key={index} className="playlistsList">
                   {console.log("playlist: ", song)}
 
-                  <div className="songItem" style={{ height: "50px" }}>
-                    <span className="songFont" style={{ color: "black" }}>
-                      {song.songName}
+                  <div
+                    className="songItem2"
+                    style={{ height: "50px", width: "100%" }}
+                  >
+                    <span className="songFont2">
+                      {song.songName} - {song.songArtist}
                     </span>
                   </div>
                 </div>
